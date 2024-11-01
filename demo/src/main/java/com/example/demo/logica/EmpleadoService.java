33ac989e7d565dc36d9b14ae95075aa538b25731
MateoGC-04 <mateogonzalezcano04@gmail.com>
2024-10-31 23:26:22 -0500

    updated For serverless

--- demo/src/main/java/com/example/demo/logica/EmpleadoService.java
+++ demo/src/main/java/com/example/demo/logica/EmpleadoService.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.validation.*;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Set;
 
@@ -22,9 +25,43 @@ public class EmpleadoService {
         if (validateEmpleado(empleado)){
             empleado.setId(getMaxId(findAllEmpleados())+1);
             repository.save(empleado);
+
+            invokeLambdaApi(empleado.getNombre(), empleado.getApellido(), empleado.getEmail(), empleado.getTelefono(), "mateogonzalezcano04@gmail.com");
             return true;
         }
             return false;
+    }
+
+    private void invokeLambdaApi(String nombre, String apellido, String email, String telefono, String recipient) {
+        try {
+            URL url = new URL("https://gi6tx90uk0.execute-api.us-east-2.amazonaws.com/default/Welcome");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/json");
+            connection.setDoOutput(true);
+
+            // Crea el payload para enviar la información en JSON
+            String jsonInputString = String.format(
+                    "{\"nombre\": \"%s\", \"apellido\": \"%s\", \"recipient\": \"%s\", \"email\": \"%s\", \"telefono\": \"%s\"}",
+                    nombre, apellido, recipient, email, telefono);
+
+            // Envía el payload
+            System.out.println(connection.getOutputStream());
+            try (OutputStream os = connection.getOutputStream()) {
+                byte[] input = jsonInputString.getBytes("utf-8");
+                os.write(input, 0, input.length);
+            }
+            // Lee la respuesta
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                System.out.println("Respuesta de Lambda API: " + responseCode);
+            } else {
+                System.err.println("Error en la respuesta de Lambda API: " + responseCode);
+            }
+
+        } catch (Exception e) {
+            System.err.println("Error al invocar la API de Lambda: " + e.getMessage());
+        }
     }
 
     public List<EmpleadoORM> findAllEmpleados(){
